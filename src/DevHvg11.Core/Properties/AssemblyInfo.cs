01ef84ea9a4cf781845df9140226310bbb6f2185
felegy <felegy@gmail.com>
2015-07-28 15:18:47 +0200

    Initial

---
+++ src/DevHvg11.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,43 @@
+﻿#region Copyright
+//  --------------------------------------------------------------------------------------------------------------------
+//  <copyright file="AssemblyInfo.cs" company="HVG Kiadó ZRT">
+//   Copyright (c) HVG Kiadó ZRT. All rights reserved.
+//  </copyright>
+//  --------------------------------------------------------------------------------------------------------------------
+#endregion
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DevHvg11.Core")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DevHvg11.Core")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a96449cf-a0d1-4ddf-87f3-51528cc14556")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/DevHvg11.apphb.com/default.aspx
@@ -0,0 +1,33 @@
+﻿<%@Page Language="C#" %><%@ Import Namespace="System.IO" %><!DOCTYPE html>
+<html>
+	<title><%# ReadMePath%></title>
+	<xmp theme="united" style="display: none;">
+<%# ReadMe%>
+	</xmp>
+	<script src="http://strapdownjs.com/v/0.2/strapdown.js"></script>
+</html>
+<script runat="server">
+	public static readonly string ReadMePath = "README.md";
+	
+	public static string ReadMe;
+	
+	protected override void OnInit(EventArgs eventArgs)
+	{	
+		if (string.IsNullOrEmpty(ReadMe))
+		{
+			var absolutePath = this.Server.MapPath(ReadMePath);
+			if (File.Exists(absolutePath))
+			{
+				ReadMe = File.ReadAllText(absolutePath);
+			}
+		}
+		
+		base.OnInit(eventArgs);
+	} 
+	
+	protected override void OnLoad(EventArgs eventArgs)
+	{
+		base.OnLoad(eventArgs);
+		this.DataBind();
+	}
+</script>
