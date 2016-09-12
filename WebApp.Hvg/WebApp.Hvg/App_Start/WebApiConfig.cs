115de566adebcc6d4b38bbcc3abccec884284759
devhvg11 <dev.hvg.11@gmail.com>
2016-09-12 19:10:20 +0200

    $39:https://trello.com/c/AhfFpO81 Task solution

---
+++ WebApp.Hvg/WebApp.Hvg/App_Start/WebApiConfig.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Http;
+
+namespace WebApp.Hvg
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        { 
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "SearchCard", 
+                routeTemplate: "Search/Card/sha1id",
+                defaults: new { sha1id = (string)null });
+
+            config.Routes.MapHttpRoute(
+                name: "SearchCommit",
+                routeTemplate: "Search/Commit/shorturl",
+                defaults: new { shorturl = (string)null });
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "Search/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ WebApp.Hvg/WebApp.Hvg/Global.asax.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Http;
+using Manatee.Trello;
+using Manatee.Trello.ManateeJson;
+using Manatee.Trello.RestSharp;
+
+namespace WebApp.Hvg
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            var serializer = new ManateeSerializer();
+            TrelloConfiguration.Serializer = serializer;
+            TrelloConfiguration.Deserializer = serializer;
+            TrelloConfiguration.JsonFactory = new ManateeFactory();
+            TrelloConfiguration.RestClientProvider = new RestSharpClientProvider();
+
+            TrelloAuthorization.Default.AppKey = Properties.Settings.Default.TrelloAppKey;
+            TrelloAuthorization.Default.UserToken = Properties.Settings.Default.TrelloUserToken;
+        }
+    }
+}
